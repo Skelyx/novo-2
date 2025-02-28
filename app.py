657aa1b873558bfbb8f0002cba7fde54fef8acd9
Skelyx <158779244+Skelyx@users.noreply.github.com>
2025-02-28 15:47:07 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Povezivanje sa MySQL bazom
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/flask_app_db'
+# Povezivanje sa MySQL bazom na Renderu
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@my-database-instance.onrender.com:3306/flask_app_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -17,25 +17,30 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
-# Ruta za početnu stranicu
+# Homepage route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Ruta za prijavu
+# Submit route za login formu
 @app.route('/submit', methods=['POST'])
 def submit():
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-    # Dodaj korisnika u bazu
-    user = User(username=username, password=password)
-    db.session.add(user)
-    db.session.commit()
+    # Provera da li korisnik postoji u bazi
+    user = User.query.filter_by(username=username, password=password).first()
+    if user:
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('index.html', message="Invalid credentials, try again.")
 
-    # Prikazivanje poruke
-    return render_template('index.html', message="Uspješno ste se prijavili!")
+# Dashboard route nakon uspešnog logovanja
+@app.route('/dashboard')
+def dashboard():
+    return "Welcome to the dashboard!"
 
+# Pokretanje aplikacije
 if __name__ == '__main__':
-    db.create_all()
+    db.create_all()  # Kreira sve tabele ako ne postoje
     app.run(host="0.0.0.0", port=10000, debug=True)
